f22388fc3a56d0143952ba50fb597b12938dfe59
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2023-09-22 00:18:12 +0100

    refactor the tracks and artists views

---
+++ apps/spotify/views/base.py
@@ -0,0 +1,42 @@
+import requests
+from requests import Response
+from rest_framework.views import APIView
+from apps.spotify.models import TimeFrame
+
+
+class SpotifyAPIView(APIView):
+    spotify_endpoint = None
+
+    def get(self, request):
+        access_token = request.user.spotifytoken.access_token
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        time_frame = request.GET.get("time_frame", "medium_term")
+        time_frame_map = {
+            "4 weeks": TimeFrame.SHORT_TERM,
+            "6 months": TimeFrame.MEDIUM_TERM,
+            "lifetime": TimeFrame.LONG_TERM,
+        }
+        time_frame = time_frame_map.get(time_frame, TimeFrame.MEDIUM_TERM)
+
+        try:
+            limit = int(request.GET.get("limit", 20))
+        except ValueError:
+            limit = 10
+
+        response = requests.get(
+            self.spotify_endpoint,
+            headers=headers,
+            params={"limit": limit, "time_range": time_frame.value},
+        )
+
+        if response.status_code == 200:
+            return self.handle_response(response, time_frame)
+        else:
+            return Response(
+                {"error": f"Failed to retrieve data from {self.spotify_endpoint}"},
+                status=response.status_code,
+            )
+
+    def handle_response(self, response, time_frame):
+        raise NotImplementedError
--- apps/spotify/views/tracks.py
+++ apps/spotify/views/tracks.py
@@ -1,102 +1,67 @@
-import requests
 from django.contrib.auth.decorators import login_required
 from django.db.models import Max
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
+
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from apps.spotify.models import Artist, TimeFrame, TopTracks, Track
+from apps.spotify.models import Artist, Track, Genre, TimeFrame, TopTracks
 from apps.spotify.serializers import TrackSerializer
-
+from apps.spotify.views.base import SpotifyAPIView
 
 @method_decorator(login_required, name="dispatch")
-class TopTracksView(APIView):
-    def get(self, request):
-        access_token = request.user.spotifytoken.access_token
-        headers = {"Authorization": f"Bearer {access_token}"}
+class TopTracksView(SpotifyAPIView):
+    spotify_endpoint = "https://api.spotify.com/v1/me/top/tracks"
 
-        time_frame = request.GET.get("time_frame", "medium_term")
-        time_frame_map = {
-            "4 weeks": TimeFrame.SHORT_TERM,
-            "6 months": TimeFrame.MEDIUM_TERM,
-            "lifetime": TimeFrame.LONG_TERM,
-        }
-        time_frame = time_frame_map.get(time_frame, TimeFrame.MEDIUM_TERM)
+    def handle_response(self, response, time_frame):
+        top_tracks_data = response.json().get("items", [])
+        tracks = []
 
-        try:
-            limit = int(request.GET.get("limit", 20))
-        except ValueError:
-            limit = 10
+        for order, track_data in enumerate(top_tracks_data):
+            track, _ = Track.objects.get_or_create(
+                song_id=track_data["id"],
+                defaults={
+                    "name": track_data["name"],
+                    "img_url": track_data["album"]["images"][0]["url"],
+                },
+            )
 
-        limit = min(max(1, limit), 50)
-
-        response = requests.get(
-            "https://api.spotify.com/v1/me/top/tracks",
-            headers=headers,
-            params={
-                "time_range": time_frame.value,
-                "limit": limit,
-            },
-        )
-
-        if response.status_code == 200:
-            top_tracks_data = response.json().get("items", [])
-            tracks = []
-
-            for _, track_data in enumerate(top_tracks_data):
-                track, created = Track.objects.get_or_create(
-                    song_id=track_data["id"],
+            for artist_data in track_data["artists"]:
+                artist, _ = Artist.objects.get_or_create(
+                    spotify_id=artist_data["id"],
                     defaults={
-                        "name": track_data["name"],
-                        "img_url": track_data["album"]["images"][0]["url"],
+                        "name": artist_data["name"],
                     },
                 )
+                track.artists.add(artist)
 
-                if (
-                    not created
-                    and track.img_url != track_data["album"]["images"][0]["url"]
-                ):
-                    track.img_url = track_data["album"]["images"][0]["url"]
-                    track.save()
+            tracks.append(track)
 
-                for artist_data in track_data["artists"]:
-                    artist, _ = Artist.objects.get_or_create(
-                        spotify_id=artist_data["id"], name=artist_data["name"]
-                    )
-                    track.artists.add(artist)
+        Track.objects.bulk_create(tracks, ignore_conflicts=True)
 
-                tracks.append(track)
+        max_order = (
+            TopTracks.objects.filter(user=self.request.user)
+            .filter(time_frame=time_frame)
+            .aggregate(max_order=Max("order"))
+            .get("max_order")
+            or 0
+        )
 
-            Track.objects.bulk_create(tracks, ignore_conflicts=True)
-
-            max_order = (
-                TopTracks.objects.filter(user=request.user)
-                .filter(time_frame=time_frame)
-                .aggregate(max_order=Max("order"))
-                .get("max_order")
-                or 0
+        top_tracks = [
+            TopTracks(
+                user=self.request.user,
+                track=track,
+                time_frame=time_frame,
+                order=max_order + order + 1,
             )
+            for order, track in enumerate(tracks)
+        ]
 
-            top_tracks = [
-                TopTracks(
-                    user=request.user,
-                    track=track,
-                    time_frame=time_frame,
-                    order=max_order + order + 1,
-                )
-                for order, track in enumerate(tracks)
-            ]
+        TopTracks.objects.bulk_create(top_tracks)
 
-            TopTracks.objects.bulk_create(top_tracks)
-
-            serializer = TrackSerializer(tracks, many=True)
-            if request.accepted_renderer.format == "html":
-                context = {"tracks": serializer.data}
-                return render(request, "top_tracks.html", context)
-            else:
-                return Response(serializer.data)
+        serializer = TrackSerializer(tracks, many=True)
+        if self.request.accepted_renderer.format == "html":
+            context = {"tracks": serializer.data}
+            return render(self.request, "top_tracks.html", context)
         else:
-            return Response(
-                {"error": "Failed to retrieve top tracks"}, status=response.status_code
-            )
+            return Response(serializer.data)
