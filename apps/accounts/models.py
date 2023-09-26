4c624d439df69e33edf2dbcef27d32c02efc13c4
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2023-09-27 00:07:26 +0100

    create privacy settings automatically after user creation

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -20,7 +20,15 @@ class PrivacySettings(models.Model):
     show_top_genres = models.BooleanField(default=True)
     show_top_albums = models.BooleanField(default=True)
     show_streams = models.BooleanField(default=True)
-    show_streams_stats = models.Boolean(default=True)
+    show_streams_stats = models.BooleanField(default=True)
     leaderboards = models.BooleanField(default=True)
     show_friends = models.BooleanField(default=True)
     show_connections = models.BooleanField(default=True)
+
+
+class Friendship(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="friendships")
+    friend = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="friend_of")
+
+    def __str__(self):
+        return f"{self.user} - {self.friend}"
