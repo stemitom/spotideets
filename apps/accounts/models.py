3be8a26f037f0cd9ad47fa0f491170037ace95be
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2023-09-28 12:14:57 +0100

    new user fields like bio and username for user operations

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     spotify_user_email = models.EmailField(unique=True)
     spotify_user_id = models.CharField(max_length=255, unique=True)
     bio = models.TextField(
@@ -19,18 +19,18 @@ class CustomUser(AbstractUser):
 
 class PrivacySettings(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    show_streams = models.BooleanField(default=True)
+    leaderboards = models.BooleanField(default=True)
+    show_friends = models.BooleanField(default=True)
     public_profile = models.BooleanField(default=True)
     current_playing = models.BooleanField(default=True)
     recently_played = models.BooleanField(default=True)
     show_top_tracks = models.BooleanField(default=True)
-    show_top_artists = models.BooleanField(default=True)
     show_top_genres = models.BooleanField(default=True)
     show_top_albums = models.BooleanField(default=True)
-    show_streams = models.BooleanField(default=True)
-    show_streams_stats = models.BooleanField(default=True)
-    leaderboards = models.BooleanField(default=True)
-    show_friends = models.BooleanField(default=True)
     show_connections = models.BooleanField(default=True)
+    show_top_artists = models.BooleanField(default=True)
+    show_streams_stats = models.BooleanField(default=True)
 
 
 class Friendship(models.Model):
