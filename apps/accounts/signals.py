cc309ae06fe608ed86b1d27f0c99f1eab2c17631
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2023-10-05 09:29:03 +0100

    oops, signal gone

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
-from .models import PrivacySettings
+# from .models import PrivacySettings
 
 
-@receiver(post_save, sender=User)
-def create_privacy_settings(sender, instance, created, **kwargs):
-    if created:
-        PrivacySettings.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_privacy_settings(sender, instance, created, **kwargs):
+#     if created:
+#         PrivacySettings.objects.create(user=instance)
